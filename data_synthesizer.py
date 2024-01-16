707b772f949279f9f3febdcf77f6674defc15141
Jay Paun <107053440+Jay2704@users.noreply.github.com>
2024-01-15 19:38:29 -0500

    Update data_synthesizer.py

--- data_synthesizer.py
+++ data_synthesizer.py
@@ -1,44 +1,34 @@
-import json
-from faker import Faker
+# DataSynthesizer.py
+from data_loader import DataLoader
+from data_analyzer import DataAnalyzer
+from model_generator import ModelGenerator
+from data_saver import DataSaver
 
+class DataSynthesizer:
+    def __init__(self):
+        self.data_loader = DataLoader()
+        self.data_analyzer = DataAnalyzer()
+        self.model_generator = ModelGenerator()
+        self.data_saver = DataSaver()
 
+    def load_data(self):
+        raw_data = self.data_loader.read_data()
+        return self.data_loader.parse_data(raw_data)
 
-def generate_fake_data(schema, num_instances):
-    fake = Faker()
-    fake_data_list = []
-    
-    for _ in range(num_instances):
-        fake_instance = {}
-        for field, field_type in schema.items():
-            if field_type == 'name':
-                fake_instance[field] = fake.name()
-            elif field_type == 'address':
-                fake_instance[field] = fake.address()
-            elif field_type == 'email':
-                fake_instance[field] = fake.email()
+    def analyze_data(self, data):
+        self.data_analyzer.analyze_distributions(data)
+        self.data_analyzer.identify_relationships(data)
+        self.data_analyzer.detect_sensitive_attributes(data)
 
-        fake_data_list.append(fake_instance)
+    def generate_model(self, data):
+        model_type = self.model_generator.select_model_type()
+        self.model_generator.train_model(data, model_type)
 
-    return fake_data_list
+    def generate_synthetic_data(self, model_type):
+        synthetic_data = self.model_generator.generate_samples(model_type)
+        return synthetic_data
 
-
-
-
-def main():
-    
-    input_schema = input("Enter the schema in JSON format: ")
-
-    schema = json.loads(input_schema)
-
-    num_instances = int(input("Enter the number of instances: ")) 
-
-    
-    fake_data = generate_fake_data(schema, num_instances)
-
-   
-    with open('fake_data.json', 'w') as file:
-        json.dump(fake_data, file, indent=4)
-
-if __name__ == "__main__":
-    main()
-
+    def save_data(self, data):
+        file_path = self.data_saver.save_data_to_file(data)
+        self.data_saver.save_data_to_database(data)
+        return file_path
